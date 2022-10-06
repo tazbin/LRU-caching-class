d6556d210085c2ff05af9d475ad0b5fc7c082fb7
tazbin <md.tazbinur.rahaman@gmail.com>
2022-10-06 07:13:08 +0600

    cache implemeted

---
+++ app.js
@@ -0,0 +1,52 @@
+// class definiton
+class LRUCache {
+
+    constructor(capacity) {
+        this.capacity = capacity; // size of the cache
+        this.map = new Map(); // cache memory
+    }
+
+    
+    get(key) {
+
+        if (this.map.has(key)) { // key exists
+
+
+            let value = this.map.get(key);
+
+            // delete the pair
+            this.map.delete(key);
+
+            // add the pair to most recent
+            this.map.set(key, value);
+
+            return this.map.get(key);
+
+        } else { // key doesn't exists
+            
+            return -1;
+        }
+
+    }
+
+    put(key, value) {
+
+        if (this.map.has(key)) { // pair exists
+
+            // delete this pair
+            this.map.delete(key);
+
+        } else if (this.capacity === this.map.size) { // pair exists, and map is full
+
+            // delete the pair from first of cache i.e. least recent
+            this.map.delete(this.map.keys().next().value);
+
+        }
+
+        // add the pair to most recent
+        this.map.set(key, value);
+
+    }
+};
+
+module.exports = LRUCache
---
+++ app.test.js
@@ -0,0 +1,73 @@
+const LRUCache = require('./app');
+
+describe('LRU cache test suit', () => {
+
+
+    test('get(1) --> should return 1', () => {
+
+        let lRUCache = new LRUCache(2);
+        lRUCache.put(1, 1);
+        lRUCache.put(2, 2);
+        result = lRUCache.get(1);
+
+        expect(result).toBe(1);
+    });
+
+    test('get(2) --> should return -1', () => {
+
+        let lRUCache = new LRUCache(2);
+        lRUCache.put(1, 1);
+        lRUCache.put(2, 2);
+        lRUCache.get(1);
+        lRUCache.put(3, 3);
+        result = lRUCache.get(2);
+
+        expect(result).toBe(-1);
+    });
+
+    test('get(1) --> should return -1', () => {
+
+        let lRUCache = new LRUCache(2);
+        lRUCache.put(1, 1);
+        lRUCache.put(2, 2);
+        lRUCache.get(1);
+        lRUCache.put(3, 3);
+        lRUCache.get(2);
+        lRUCache.put(4, 4);
+        result = lRUCache.get(1);
+
+        expect(result).toBe(-1);
+    });
+
+    test('get(3) --> should return 3', () => {
+
+        let lRUCache = new LRUCache(2);
+        lRUCache.put(1, 1);
+        lRUCache.put(2, 2);
+        lRUCache.get(1);
+        lRUCache.put(3, 3);
+        lRUCache.get(2);
+        lRUCache.put(4, 4);
+        lRUCache.get(1);
+        result = lRUCache.get(3);
+
+        expect(result).toBe(3);
+    });
+
+    test('get(4) --> should return 4', () => {
+
+        let lRUCache = new LRUCache(2);
+        lRUCache.put(1, 1);
+        lRUCache.put(2, 2);
+        lRUCache.get(1);
+        lRUCache.put(3, 3);
+        lRUCache.get(2);
+        lRUCache.put(4, 4);
+        lRUCache.get(1);
+        lRUCache.get(3);
+        result = lRUCache.get(4);
+
+        expect(result).toBe(4);
+    });
+
+});
